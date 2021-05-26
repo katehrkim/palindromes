ba2fde46dc9a4ed712ad2ccadd7a881387c68b21
Kate Kim <katehrkim@gmail.com>
2021-05-26 19:57:19 -0400

    Kate's answer

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,15 @@
 def palindrome(word):
-    # Write code here
+    if type(word) == str:
+        word = word.lower()
+    if type(word) == int:
+        word = str(word)
+    punctuations = '''!() -[]{};:'"\,<>./?@#$%^&*_~'''
+    cleaned = ""
+    for char in word:
+        if char not in punctuations:
+            cleaned = cleaned + char
+    half_len = len(cleaned) // 2
+    for i in range(0, half_len):
+        if cleaned[i] != cleaned[len(cleaned) - 1 - i]:
+            return False
+    return True
--- python/palindrome_spec.py
+++ python/palindrome_spec.py
@@ -1,15 +1,28 @@
 # Can you translate this driver code to unit tests?
-
+import unittest
 from palindrome import palindrome
 # This should return a bunch of trues
-print(palindrome('racecar') == True)
-print(palindrome('Noon') == True)
-print(palindrome('ciVic') == True)
-print(palindrome('nice') == False)
-print(palindrome(434) == True)
-print(palindrome(123) == False)
-print(palindrome('bomb') == False)
+class PalindromeTestCase(unittest.TestCase):
+    """Tests for palindrome.py"""
+    def test_one(self):
+        self.assertEqual(palindrome('racecar'), True)
+    def test_two(self):
+        self.assertEqual(palindrome('Noon'), True)
+    def test_three(self):
+        self.assertEqual(palindrome('ciVic'), True)
+    def test_four(self):
+        self.assertEqual(palindrome('nice'), False)
+    def test_five(self):
+        self.assertEqual(palindrome(434), True)
+    def test_six(self):
+        self.assertEqual(palindrome(123), False)
+    def test_seven(self):
+        self.assertEqual(palindrome('bomb'), False)
+    # The following should be True if you're trying to do the extra portion of this challenge
+    def test_eight(self):
+        self.assertEqual(palindrome('Sore was I ere I saw Eros.'), True)
+    def test_nine(self):
+        self.assertEqual(palindrome('A man, a plan, a canal -- Panama'), True)
 
-print("The following should be True if you're trying to do the extra portion of this challenge")
-print(palindrome('Sore was I ere I saw Eros.') == True)
-print(palindrome('A man, a plan, a canal -- Panama') == True)
+    if __name__ == '__main__':
+        unittest.main()
